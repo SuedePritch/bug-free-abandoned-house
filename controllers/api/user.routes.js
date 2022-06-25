38bfcd4dc4e532840965b4370a21538642d4e7ba
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-25 18:07:34 -0400

    signup and login api routes

--- controllers/api/user.routes.js
+++ controllers/api/user.routes.js
@@ -1,13 +1,58 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const User = require('../../models/User')
 
-  router.get('/', async (req, res) => {
-    const userData = await User.findAll().catch((err) => { 
-        res.json(err);
-      });
-        const users = userData.map((user) => user.get({ plain: true }));
-        res.render('homepage', { users });
-      });
+//GET ALL USERS
+router.get('/', async (req, res) => {
+  const userData = await User.findAll().catch((err) => { 
+      res.json(err);
+    });
+      const users = userData.map((user) => user.get({ plain: true }));
+      res.status(200).json(userData)
+      res.render('homepage', { users });
+    });
+
+//USER SIGNUP
+router.post('/signup', async (req, res) => {
+  try {
+    const signupData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+
+
+//USER LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const loginAttempt = await User.findOne({ where: { email: req.body.email } });
+    if (!loginAttempt) {
+
+      res.status(404).json({ message: 'Thats not working! Please try again!' });
+      return;
+    }
+    console.log(req.body.password);
+    console.log(loginAttempt.password);
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      loginAttempt.password
+    );
+    if (!validPassword) {
+      res.status(400).json({ message: 'Login failed. Please try again!' });
+      return;
+    }
+    res.status(200).json({ message: 'You are now logged in!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+    
 
 
 
--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPass) {
+        return bcrypt.compareSync(loginPass, this.password);
+    }
+}
 
 User.init(
     {
@@ -14,17 +19,37 @@ User.init(
     username: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            isAlphanumeric: true
+        } 
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique:true,
+        validate: {
+            isEmail:true
+        } 
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+            len :[8,26],
+        }  
     },
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate (updateUserData) {
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                return updateUserData;
+            }
+        },
     sequelize,
     freezeTableName: true,
     underscored: true,
