ee62ae7d66d20983fce6125a59795c735f5b9785
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-24 20:57:55 -0400

    post model and seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,11 @@
 const sequelize = require('../config/connection');
-const Dish = require('../models/Dish');
-const dishData = require('./dish-seeds.json');
+const Post = require('../models/Post');
+const postData = require('./post-seed.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Dish.bulkCreate(dishData, {
-    individualHooks: true,
-    returning: true,
-  });
+  await Post.bulkCreate(postData);
 
   process.exit(0);
 };
