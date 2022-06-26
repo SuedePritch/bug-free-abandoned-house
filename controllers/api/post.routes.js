ca7bd1df7d650ecaee7bd882f3d0c74940bf48c6
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-26 16:51:48 -0400

    new post form

--- controllers/api/post.routes.js
+++ controllers/api/post.routes.js
@@ -6,10 +6,24 @@ router.get('/', async (req, res) => {
       res.json(err);
     });
       const posts = postData.map((post) => post.get({ plain: true }));
-      // res.status(200).json(posts)
-      res.render('homepage', { posts });
+      res.status(200).json(posts)
+      // res.render('homepage', { posts });
     });
 
+
+    router.post('/', async (req, res) => {
+      try {
+        const newPostData = await Post.create({
+          title: req.body.title,
+          content: req.body.content,
+          user_id: req.body.user_id,
+        });
+        res.status(200).json('new Post created')
+      } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+      }
+    });
 
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -6,5 +6,6 @@ router.use('/api', apiRoutes);
 router.get('/', async (req, res) => {res.render('homepage');});
 router.get('/login', async (req, res) => {res.render('login');});
 router.get('/signup', async (req, res) => {res.render('signup');});
+router.get('/newpost', async (req, res) => {res.render('newPost');});
 
 module.exports = router;
