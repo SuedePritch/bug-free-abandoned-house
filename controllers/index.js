9cfa1e53e9c54daafec1d1bca4914f1ba15c104a
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-27 17:43:25 -0400

    posts have username and date

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,22 @@
 const router = require('express').Router();
-const Post = require('../models/Post')
+const { Post, User} = require('../models')
 const withAuth = require('../utils/auth')
 
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.get('/', async (req, res) => {
-    const postData = await Post.findAll().catch((err) => { 
+    const postData = await Post.findAll({
+        include: [{   
+            model: User,
+            attributes: ['username'], 
+        }]
+    }).catch((err) => { 
         
         res.json(err);
     });
-        const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // res.status(200).json(posts)
         res.render('homepage', { 
             posts, 
             loggedIn: req.session.loggedIn
@@ -20,10 +26,14 @@ router.get('/', async (req, res) => {
 router.get('/login', async (req, res) => {res.render('login');});
 router.get('/signup', async (req, res) => {res.render('signup');});
 router.get('/newpost', withAuth, async (req, res) => {res.render('newPost');});
-router.get('/singlepost/:id', async (req, res) => {
+router.get('/singlepost/:id', withAuth, async (req, res) => {
     try {
         const singlePostData = await Post.findByPk(
-        req.params.id)
+        req.params.id,{
+        include: [{   
+            model: User,
+            attributes: ['username'], 
+        }]})
         const singlePost = singlePostData.get({plain:true})
         res.render('singlepost', { singlePost,
             loggedIn: req.session.loggedIn
