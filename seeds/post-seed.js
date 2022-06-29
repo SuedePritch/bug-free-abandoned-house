ae3bcdbef26042164bc15a00c2a86f0ed311cd3d
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-28 20:01:51 -0400

    added jawsDB

--- seeds/post-seed.js
+++ seeds/post-seed.js
@@ -2,22 +2,22 @@ const { Post } = require('../models')
 
 const postData = [
     {
-    "title": "NodeJS",
-    "content": "Lorem ipsum",
+    "title": "Are You Prepared for Digital Currency.",
+    "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus natus, odit eos a impedit fugit nostrum quo adipisci accusantium nihil, veniam nesciunt ducimus nobis provident earum nulla libero minus eum.",
     "user_id": 1
     },
     {
-    "title": "Javascript",
-    "content": "Lorem ipsum",
+    "title": "The Autonomous Vehicle.",
+    "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quo, maiores ipsam eos iure aut rem. Quaerat sapiente doloremque commodi hic eligendi. Rerum veniam officiis, aperiam veritatis praesentium illo saepe explicabo voluptatibus blanditiis! ",
     "user_id": 2,
     },
     {
-    "title": "MERN",
-    "content": "Lorem ipsum",
+    "title": "9 Amazing Computer Inventions from the Past Decade",
+    "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam pariatur, voluptatem id, iste, molestias molestiae eveniet aspernatur eaque enim vel ipsum hic nemo cupiditate rerum vero quod nulla eligendi. Expedita, velit. Ipsam veritatis.",
     "user_id": 3,
     },
     {
-    "title": "Express",
+    "title": "Why Silicon Valley Should Move To The South",
     "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolorum molestiae nemo, soluta doloribus nisi doloremque sapiente! Eius quibusdam enim hic nam, architecto iusto temporibus?",
     "user_id": 3,
     }
@@ -25,3 +25,4 @@ const postData = [
 const seedPosts = () => Post.bulkCreate(postData);
 
 module.exports = seedPosts;
+
